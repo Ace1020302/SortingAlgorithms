44f90099875e5fbd10c63e3396c9774f54310d9f
Ace1020302 <phillip11ace@gmail.com>
2023-02-11 08:33:59 -0500

    Added Quicksort class and new methods

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -1,10 +1,71 @@
 ﻿namespace SortingAlgorithms;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("This should show up in github!!!");
-        Console.WriteLine("This should show up TOO!!!");
+        const string _filePath = @"../../../inputJagged.csv";
+        int[][] jaggedArray =  new int[20][];
 
+        //Console.ReadLine(File.OpenRead(_filePath));
+
+        //Console.Write(File.ReadLines(_filePath).Where(lineToCheck => lineToCheck != null).Select(lineToSplit => lineToSplit.Split(',')));
+
+        //SOURCE THIS!!!
+        try
+        {
+            // Create an instance of StreamReader to read from a file.
+            // The using statement also closes the StreamReader.
+            using (StreamReader sr = new StreamReader(_filePath))
+            {
+                string line;
+                int idxOfArr = 0;
+                
+
+                while ((line = sr.ReadLine()!) != null)
+                {
+                    jaggedArray[idxOfArr] = ConvertLineToInt(line);
+                    idxOfArr++;
+                }       
+            }
+        }
+        catch (Exception e)
+        {
+            // Let the user know what went wrong.
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
+
+        PrintJaggedArray(jaggedArray);
+
+    }
+
+
+    static private int[] ConvertLineToInt(string line)
+    {
+        // splits the line where there are commas
+        string[] lineParts = line.Split(',');
+
+        // To store new array of integers
+        int[] intArray = new int[lineParts.Length];
+
+        // Converts string to numbers
+        for (int i = 0; i < lineParts.Length; i++)
+            int.TryParse(lineParts[i], out intArray[i]);
+
+        // send back an array of those numbers
+        return intArray;
+    }
+
+    static private void PrintJaggedArray(int[][] jagArr)
+    {
+        // Simply prints out array
+        foreach (int[] arr in jagArr)
+        {
+            foreach (int num in arr)
+                Console.Write($"{num} | ");
+            Console.WriteLine();
+        }
     }
 }
---
+++ SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace SortingAlgorithms
+{
+	public class QuickSort
+	{
+		// An array needs to be put in here
+		// array needs to be sorted here
+		// array needs to be send back
+
+		public int[] QuickSortArray(int[] ArrToBeSorted)
+        {
+			// check if big enough to sort
+			if (ArrToBeSorted.Length <= 1)
+				return ArrToBeSorted;
+
+            // pick a pivot
+            // int pivotIdx = pickPivot();
+            int pivotIdx = ArrToBeSorted.Length;
+
+            // swap pivot to end (if needed)
+            // swap(pivotIdx, endIdx);
+            // pivotIdx = endIdx;
+
+
+            // place higher values than pivot to the right side of pivot
+            for (int i = ArrToBeSorted.Length-1; i > 0; i--)
+            {
+                if (ArrToBeSorted[i] > ArrToBeSorted[pivotIdx])
+                {
+                    //swap(pivotIdx, i)
+                    //pivotIdx = i
+                }
+            }
+
+            return ArrToBeSorted;
+		}
+	}
+}
+
